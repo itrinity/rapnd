bb9d30f0c26c36fb6c05951bfd21ebb60e4e2a25
Peter Hrbacik <info@itrinity.com>
2013-12-05 21:04:07 +0000

    Fixed logger

--- lib/rapnd/client.rb
+++ lib/rapnd/client.rb
@@ -12,28 +12,28 @@ module Rapnd
     end
 
     def connect!
-      Rapnd.logger.info 'Connecting...'
+      @logger.info 'Connecting...'
 
       cert = self.setup_certificate
       @socket = self.setup_socket(cert)
 
-      Rapnd.logger.info 'Connected!'
+      @logger.info 'Connected!'
 
       @socket
     end
 
     def setup_certificate
-      Rapnd.logger.info 'Setting up certificate...'
+      @logger.info 'Setting up certificate...'
       @context      = OpenSSL::SSL::SSLContext.new
       @context.cert = OpenSSL::X509::Certificate.new(File.read(@cert))
       @context.key  = OpenSSL::PKey::RSA.new(File.read(@cert), @password)
-      Rapnd.logger.info 'Certificate created!'
+      @logger.info 'Certificate created!'
 
       @context
     end
 
     def setup_socket(ctx)
-      Rapnd.logger.info 'Connecting...'
+      @logger.info 'Connecting...'
 
       socket_tcp = TCPSocket.new(@host, 2195)
       OpenSSL::SSL::SSLSocket.new(socket_tcp, ctx).tap do |s|
@@ -55,24 +55,24 @@ module Rapnd
 
     def push(notification)
       begin
-        Rapnd.logger.info "Sending #{notification.device_token}: #{notification.json_payload}"
+        @logger.info "Sending #{notification.device_token}: #{notification.json_payload}"
         socket.write(notification.to_bytes)
         socket.flush
 
         if IO.select([socket], nil, nil, 1) && error = socket.read(6)
           error = error.unpack('ccN')
-          Rapnd.logger.error "Encountered error in push method: #{error}, backtrace #{error.backtrace}"
+          @logger.error "Encountered error in push method: #{error}, backtrace #{error.backtrace}"
           return false
         end
 
-        Rapnd.logger.info 'Message sent'
+        @logger.info 'Message sent'
 
         true
       rescue OpenSSL::SSL::SSLError, Errno::EPIPE => e
-        Rapnd.logger.error "Encountered error: #{e}, backtrace #{e.backtrace}"
-        Rapnd.logger.info 'Trying to reconnect...'
+        @logger.error "Encountered error: #{e}, backtrace #{e.backtrace}"
+        @logger.info 'Trying to reconnect...'
         reset_socket
-        Rapnd.logger.info 'Reconnected'
+        @logger.info 'Reconnected'
       end
     end
   end
